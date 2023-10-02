a241f365a5b0ef8852c8d89843457e224d98b94b
Raymond Lawson <btzlawson@yahoo.com>
2023-10-01 22:14:31 -0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "localhost";
+$host = "s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "mysqladmin";
+$username = "vpyowenwxvd0rt1o";
+$user_pass = "y666924ar7chdogc";
+$database_in_use = "eqq3vu9i1kc3tbqw";
 
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
