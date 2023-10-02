58d03702095038b30d719519495a79ad1a04587d
Raymond Lawson <btzlawson@yahoo.com>
2023-10-01 23:04:34 -0400

    Update add_joke.php

--- add_joke.php
+++ add_joke.php
@@ -14,7 +14,7 @@ $userid = $_SESSION['userid'];
 
 echo "<h2>Trying to add a new joke " . $new_joke_question . " and " . $new_joke_answer . "</h2>";
 
-$stmt = $mysqli->prepare("INSERT INTO jokes_table (Joke_ID, Joke_question, Joke_answer, users_id) VALUES (null, ?, ?, ?)");
+$stmt = $mysqli->prepare("INSERT INTO eqq3vu9i1kc3tbqw.jokes_table (Joke_ID, Joke_question, Joke_answer, users_id) VALUES (null, ?, ?, ?)");
 $stmt->bind_param("ssi", $new_joke_question, $new_joke_answer, $userid);
 
 $stmt->execute();
